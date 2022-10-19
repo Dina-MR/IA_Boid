0fd6564cda1f92a64fe5bdf79bebb76d4b60595b
DMR <77438997+Dina-MR@users.noreply.github.com>
2022-10-19 16:03:04 +0200

    Ajout de l'alignement & début de la répulsion

--- Assets/Scripts/Boid.cs
+++ Assets/Scripts/Boid.cs
@@ -5,29 +5,42 @@ using UnityEngine;
 public class Boid : MonoBehaviour
 {
     // Distances permettant d'établir la zone dans laquelle se trouve un voisin
-    public float alignDistance, repulseDistance; // attractDistance n'est peut être pas nécessaire à cause du collider
+    public float attractDistance, alignDistance, repulseDistance; 
 
     /*
      * Paramètres relatifs au mouvement individuel du boïd
      */
     // Vitesses aléatoires
-    public float speedMin, speedMax;
-    float speed;
+    public float defaultSpeed; // vitesse par défaut, invariable
+    public float speed; // vitesse variable
     float step = 0;
     // Limites
     float axisX = 0.05f;
     float axisZ = 0.05f;
 
+    // Accès au contrôleur
+    //public BoidsGenerator boidGenerator;
+
+    // Suivi ou non du joueur
+    bool isFollowingPlayer = false;
+
+    /*
+     * Positions extrêmes, pour le mouvement
+     */
+
+    // Etat du boid
+
     // Start is called before the first frame update
     void Start()
     {
         // Initialisation d'une vitesse aléatoire
-        speed = Random.Range(3f, 10f);
+        speed = defaultSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //detectDistances(boidGenerator);
         move();
     }
 
@@ -57,10 +70,42 @@ public class Boid : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, other.transform.position, step);
     }
 
+    void moveCloserAll(List<GameObject> neighbours)
+    {
+        int neighboursCount = neighbours.Count;
+        if (neighboursCount > 0)
+        {
+            // Calcul de la vitesse moyenne des voisins
+            float averageSpeed = 0;
+            foreach (GameObject neighbour in neighbours)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, neighbour.transform.position, step);
+            }
+        }
+    }
+
     // Alignement
     void moveWith(GameObject other)
     {
+        //Debug.Log("SPEED" + other.GetComponent<Boid>().speed);
+        //speed = other.GetComponent<Boid>().speed;
+    }
 
+    void moveWithAll(List<GameObject> neighbours, float speedLimiter)
+    {
+        int neighboursCount = neighbours.Count;
+        if (neighboursCount > 0)
+        {
+            // Calcul de la vitesse moyenne des voisins
+            float averageSpeed = 0;
+            foreach(GameObject neighbour in neighbours)
+            {
+                averageSpeed += neighbour.GetComponent<Boid>().speed;
+            }
+            averageSpeed /= neighboursCount;
+            // Adaptation de notre vitesse
+            speed += averageSpeed / speedLimiter;
+        }
     }
 
     // Répulsion
@@ -69,55 +114,93 @@ public class Boid : MonoBehaviour
 
     }
 
+    void moveAwayFromAll(List<GameObject> neighbours, float distanceCoefficient)
+    {
+        int neighboursCount = neighbours.Count;
+        if (neighboursCount > 0)
+        {
+            //Vector3 newDistance = Vector3.zero;
+            Vector3 positionDifference = Vector3.zero;
+            foreach (GameObject neighbour in neighbours)
+            {
+                positionDifference += transform.position - neighbour.transform.position;
+                //positionDifference.x = positionDifference.x >= 0 ? Mathf.Sqrt(repulseDistance) - positionDifference.x : -Mathf.Sqrt(repulseDistance) - positionDifference.x;
+                //positionDifference.y = positionDifference.y >= 0 ? Mathf.Sqrt(repulseDistance) - positionDifference.y : -Mathf.Sqrt(repulseDistance) - positionDifference.y;
+                //positionDifference.z = positionDifference.z >= 0 ? Mathf.Sqrt(repulseDistance) - positionDifference.z : -Mathf.Sqrt(repulseDistance) - positionDifference.z;
+                //newDistance = positionDifference;
+            }
+            positionDifference /= neighboursCount;
+            transform.Translate(positionDifference);
+        }
+    }
+
     // Distance entre ce boid et un autre
-    float distance(Transform other)
+    public float distance(Transform other)
     {
         return Vector3.Distance(transform.position, other.position);
     }
 
-    // Collision entre ce boid et un autre
+    /*
+     * 1ERE VERSION - AVEC COLLIDER
+     */
+
+    // Collision entre ce boid et le joueur
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Boid"))
+        //Collision avec le joueur
+        if (other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Coucou !" + other.gameObject.name + ", je suis " + name);
-            Debug.Log("Voyons nos distances : " + distance(other.gameObject.transform));
-
-            // Vérification de la distance, afin de déterminer le comportement à avoir
-            if(distance(other.gameObject.transform) <= repulseDistance)
-            {
-                Debug.Log("Répulsion !");
-            }
-            else if (distance(other.gameObject.transform) <= alignDistance)
-            {
-                Debug.Log("Alignons-nous !");
-            }
-            else
-            {
-                Debug.Log("Attends moi " + other.gameObject.name);
-                moveCloser(other.gameObject);
-            }
+            Debug.Log("Coucou joueur ! Je suis le boïd n° " + name);
+            moveCloser(other.gameObject);
         }
     }
 
-    private void OnTriggerStay(Collider other)
+    /*
+     * 2EME VERSION - AVEC LA LISTE DES BOIDS 
+     */
+    //void detectDistances(BoidsGenerator boidsGenerator)
+    //{
+    //    foreach(GameObject boid in boidsGenerator.boids)
+    //    {
+    //        if (boid == this)
+    //            continue;
+
+    //        float newDistance = distance(boid.transform);
+    //        adaptMovement(newDistance, boid);
+    //    }
+    //}
+
+    // Adaptation du mouvement selon la distance
+    public void adaptMovement(ref List<GameObject> neighbours)
     {
-        if (other.gameObject.CompareTag("Boid"))
+        Debug.Log("Nombre de voisins " + neighbours.Count);
+
+        // Liste des voisins par catégorie
+        List<GameObject> attractList = neighbours;
+        List<GameObject> alignList = new List<GameObject>();
+        List<GameObject> repulseList = new List<GameObject>();
+
+        foreach(GameObject neighbour in neighbours)
         {
+            float distanceWithNeighbour = distance(neighbour.transform);
             // Vérification de la distance, afin de déterminer le comportement à avoir
-            if (distance(other.gameObject.transform) <= repulseDistance)
+            if (distanceWithNeighbour <= repulseDistance)
             {
                 Debug.Log("Répulsion !");
+                repulseList.Add(neighbour);
             }
-            else if (distance(other.gameObject.transform) <= alignDistance)
+            if (distanceWithNeighbour <= alignDistance)
             {
                 Debug.Log("Alignons-nous !");
-            }
-            else
-            {
-                Debug.Log("Attends moi " + other.gameObject.name);
-                moveCloser(other.gameObject);
+                alignList.Add(neighbour);
+                //moveWith(neighbour);
             }
         }
+
+        //Mise à jour des mouvements
+        moveCloserAll(attractList); // attraction
+        moveWithAll(alignList, 1000); // alignement
+        moveAwayFromAll(repulseList, 1000);
+
     }
 }
---
+++ Assets/Scripts/BoidsGenerator.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidsGenerator : MonoBehaviour
+{
+    // Prefab d'un boid
+    public GameObject boidPrefab;
+
+    // Vitesses minimum & maximum d'un boid
+    public float speedMin, speedMax;
+
+    // Nombre & liste de boids
+    public int boidsNumber;
+    public List<GameObject> boids;
+
+    // Coordonnées extrêmes des positions des boids
+    public Vector3 minimumPosition;
+    public Vector3 maximumPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        generateRandomBoids();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateBoidsBehaviour();
+    }
+
+    /*
+     * Modification des comportements des boids
+     */ 
+    void updateBoidsBehaviour()
+    {
+        // Vérification des distances
+        foreach(GameObject boid in boids)
+        {
+            List<GameObject> closestBoids = new List<GameObject>();
+            foreach (GameObject otherBoid in boids)
+            {
+                if (otherBoid == boid)
+                    continue;
+
+                float singleDistance = boid.GetComponent<Boid>().distance(otherBoid.transform);
+                if(singleDistance <= boid.GetComponent<Boid>().attractDistance)
+                    closestBoids.Add(otherBoid);
+            }
+
+            // Changement du comportement
+            boid.GetComponent<Boid>().adaptMovement(ref closestBoids);
+        }
+    }
+
+    /*
+     * Positionnement aléatoire des boids
+     */
+    void generateRandomBoids()
+    {
+        for(int i = 0; i < boidsNumber; i++)
+        {
+            // Création
+            GameObject boid = Instantiate(boidPrefab, randomizePosition(), Quaternion.identity);
+            // Paramétrage
+            boid.name = "Boid n°" + (i + 1);
+            boid.GetComponent<Boid>().defaultSpeed = Random.Range(speedMin, speedMax);
+            //boid.GetComponent<Boid>().boidGenerator = this;
+            boids.Add(boid);
+        }
+    }
+
+    /*
+     * Générateur aléatoire de positions
+     * */
+
+    Vector3 randomizePosition()
+    {
+        return new Vector3(Random.Range(minimumPosition.x, maximumPosition.x), Random.Range(minimumPosition.y, maximumPosition.y), Random.Range(minimumPosition.z, maximumPosition.z));
+    }
+}
